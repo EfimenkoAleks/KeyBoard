d6950e57af126bef87bd5c4fba3875e54f18ae03
Aleksandr Efimenko <gazservissana.zp@gmail.com>
2021-05-14 14:19:58 +0300

    Initial Commit

---
+++ MyCustomKeybord/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyCustomKeybord
+//
+//  Created by Trainee Alex on 14.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
